e8a06eacfb78416775b47fc4370b2f7981589920
Martin-GS <contact@martin.gs>
2022-07-25 19:30:17 +0200

    WL v3 : Reorganization (folder architecture) + general style (browser style reset)

---
+++ wishlist_v3--clean_code/src/components/App/App.js
@@ -0,0 +1,20 @@
+// Import modules
+import React from 'react';
+
+// Import components
+
+// Miscellaneous
+import './App.css';
+import logo from '../../assets/images/logo.png';
+
+// Component
+function App() {
+  return (
+    <div className="App">
+      <img src={logo} className="App-logo" alt="logo WishList" />
+    </div>
+  );
+}
+
+// Export
+export default App;
